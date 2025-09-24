33e3ce7aeb716a35d3067f58f3a2f83719827a03
Disha <127115356+undeadcreature@users.noreply.github.com>
2025-09-24 05:27:00 +0000

    Initial commit changes

--- pr9.cpp
+++ pr9.cpp
@@ -7,12 +7,20 @@ int main(){
     for(int i=0;i<5;i++){
         cin>>num[i];
     }
+//--------------------------------------------
     int sum=0;
     for(int i=0;i<5;i++){
         sum+=num[i];
     }
     cout<<"sum using for loop is : "<<sum<<endl;
-
-    
+//---------------------------------------------
+    int sum2=0;
+    int i=0;
+    while(i<5){
+        sum2+=num[i];
+        i++;
+    }
+    cout<<"sum using while loop is : "<<sum2<<endl;
+    return 0;
 
 }
